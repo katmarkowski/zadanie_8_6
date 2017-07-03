4f91acd2f88fba42a0ad6df39494ab9af2e96e8c
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-03 10:02:11 +0200

    Added task: Instrukcje warunkowe w praktyce

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
- 
- 
+var value,
+    a = 20,
+    b = 5;
+value = (a * a) + (2 * a * b) - (b * b); 
+console.log('value = ' + value);
+if (value > 0) {
+    console.log('Wynik jest dodatni.');
+} else if (value < 0) {
+      console.log('Wynik jest ujemny.');
+} else {
+    console.log('Wynik jest równy 0.');
+}
